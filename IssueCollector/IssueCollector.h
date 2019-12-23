907dad932ac1e7123fb23c42e367db45dd2f7179
Suhaib <>
2019-12-23 13:14:42 +0100

    Project setup

---
+++ IssueCollector/IssueCollector.h
@@ -0,0 +1,19 @@
+//
+//  IssueCollector.h
+//  IssueCollector
+//
+//  Created by Suhaib on 23/12/2019.
+//  Copyright © 2019 Suhaib Al Saghir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IssueCollector.
+FOUNDATION_EXPORT double IssueCollectorVersionNumber;
+
+//! Project version string for IssueCollector.
+FOUNDATION_EXPORT const unsigned char IssueCollectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IssueCollector/PublicHeader.h>
+
+
